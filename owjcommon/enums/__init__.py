fa3c5fe30fc6f9343fe6c9fa533fe4a01eefe166
Sepehr Safa <safasepehr@gmail.com>
2023-12-30 11:44:12 -0500

    imported sort order in init

--- owjcommon/enums/__init__.py
+++ owjcommon/enums/__init__.py
@@ -1,6 +1,6 @@
 from .audit_type import AuditTypeEnum
 from .country import CountryChoices
 from .currency import CurrencyChoices
-from .filter import LogicalOperation, MatchType
+from .filter import LogicalOperation, MatchType, SortOrder
 from .permissions import USER_TYPE_PERMISSIONS, UserPermission
 from .user_types import UserSet, UserTypeChoices
